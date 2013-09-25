b2e83ff3b4aeb0b2a90400b4654b5f2cb9606340
wfsiew <siewwingfei@hotmail.com>
2013-09-26 00:05:31 +0800

    commit

--- MvcBootstrap/Helpers/HtmlHelper.cs
+++ MvcBootstrap/Helpers/HtmlHelper.cs
@@ -7,6 +7,14 @@ namespace MvcBootstrap.Helpers
 {
     public class HtmlHelpers
     {
+        public static Dictionary<string, object> LabelAttributes()
+        {
+            return new Dictionary<string, object>
+            {
+                { "class", "col-lg-2 control-label" }
+            };
+        }
+
         public static Dictionary<string, object> TextBoxAttributes(string placeholder)
         {
             return new Dictionary<string, object>
